9275854bd665336d46ae3008cd07c88fe79326a6
Bora Kim <a52447879@gmail.com>
2021-10-20 00:29:49 +0900

    add decription

--- javascript examples/6. 콜백함수 알아보기/main.js
+++ javascript examples/6. 콜백함수 알아보기/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-
-/* text 함수가 하는 일은, 예약을 거는 일뿐이다. 
-   <setTimeout 안의 코드>를 test함수가 실행시켜주는 것이 아니라,
-   text함수는 <setTimeout함수를 실행만> 시켜줄 뿐, 코드실행은 브라우저가 한다.
-*/ 
+/*
+    test 함수가 하는 일은, 예약을 거는 일뿐이다.
+    setTimeout 안의 코드를 test함수가 실행시켜주는 것이 아니라,
+    test함수는 setTimeout함수를 실행만 시켜줄 뿐, 코드실행은 브라우저가 한다. 
+*/
 
 {
    function test() {
